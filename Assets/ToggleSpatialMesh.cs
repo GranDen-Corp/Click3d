9dbec626b02e58bf4fb1d31ed28c2a34c8b4e82e
Chen-Yu Pao <windperson@gmail.com>
2020-12-17 23:11:37 +0800

    toggle spatial mesh

---
+++ Assets/ToggleSpatialMesh.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using UnityEngine;
+
+public class ToggleSpatialMesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Toggle()
+    {
+        var observer = CoreServices.GetSpatialAwarenessSystemDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
+        
+        switch (observer.DisplayOption)
+        {
+            case SpatialAwarenessMeshDisplayOptions.None:
+                observer.DisplayOption = SpatialAwarenessMeshDisplayOptions.Visible;
+                break;
+            case SpatialAwarenessMeshDisplayOptions.Visible:
+                observer.DisplayOption = SpatialAwarenessMeshDisplayOptions.Occlusion;
+                break;
+            case SpatialAwarenessMeshDisplayOptions.Occlusion:
+                observer.DisplayOption = SpatialAwarenessMeshDisplayOptions.Visible;
+                break;
+        }
+    }
+}
